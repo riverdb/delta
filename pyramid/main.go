600a4744045a6c131f4a378b75020d7ee38cd6ef
Eloff <dan.eloff@gmail.com>
2017-06-01 22:49:40 -0700

    Load config file.

--- pyramid/main.go
+++ pyramid/main.go
@@ -1,6 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/viper"
+)
+
+func init() {
+	viper.SetConfigName("config") // name of config file (without extension)
+	viper.AddConfigPath(".")      // optionally look for config in the working directory
+	err := viper.ReadInConfig()   // Find and read the config file
+	if err != nil {               // Handle errors reading the config file
+		fmt.Fprintf(os.Stderr, "Could not read config: %s\n", err.Error())
+	}
+}
 
 func main() {
 	fmt.Println("Hello World!")
