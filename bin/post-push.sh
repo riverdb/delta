24549ed62506e221237f202b1f9b8ab74396286d
Eloff <dan.eloff@gmail.com>
2017-06-01 20:47:56 -0700

    No such thing as a post-push h00k.

--- bin/post-push.sh
+++ bin/post-push.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+set -e
+
+git push "$@"
+
 if [ -n "${IGNORE_POST_PUSH_HOOK}" ]; then
         exit 0
 fi
 
-set -e
-
 if [ -f coverage.txt ]; then
 	bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
 fi
+
